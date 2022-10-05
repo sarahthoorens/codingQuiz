4e274a4c9dc7e1e0d7f3a5d5657112e3c05d93a9
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-04 19:46:15 -0500

    code successfully quizzes user, reduces time when wrong answer, provides score at the end

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,23 @@
+// set variables for DOM manipulation 
 const startButton = document.getElementById('start-button')
 const questionSectionElement = document.getElementById('question-section')
 const welcomeElement = document.getElementById('welcome')
 const formElement = document.getElementById('form')
-let questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+let questionElement = document.getElementById('question')
 let answer1Element = document.getElementById('answer1')
 let answer2Element = document.getElementById('answer2')
 let answer3Element = document.getElementById('answer3')
 let answer4Element = document.getElementById('answer4')
-let scoreElement = document.getElementById('score-button')
-
-
+let scoreButton = document.getElementById('score-button')
 let response = document.getElementById('response')
-let score = 0
-scoreElement.textContent = score
+let scoresList = document.getElementById('scoresList')
 
+//set score to zero
+let score = 0
+scoreButton.textContent = score
+
+//set countdown and conditions for time changes
 let countdownClock = document.getElementById('countdown-clock')
 let secondsLeft = 59;
 
@@ -23,12 +26,12 @@ function countdown() {
     secondsLeft--;
     countdownClock.innerHTML = secondsLeft + " seconds remain";
     if (secondsLeft <=0 ) {
-      countdownClock.setAttribute('class', 'hidden');
-      endPage();
+    endPage();
     }
   }, 1000);
 }
 
+// Define questions to iterate through
 let questionsList = [
     {
     question: 'Commonly used data types do NOT include: ',
@@ -77,8 +80,10 @@ let questionsList = [
 } 
 ]
 
+
 let questionCounter = 0 
 
+// Initialize game
 function startGame() {
     startButton.setAttribute('class', 'hidden');
     welcomeElement.children[0].setAttribute('class', 'hidden');
@@ -87,17 +92,18 @@ function startGame() {
     questionSectionElement.classList.remove('hidden');
     countdown();
     setNextQuestion();
-  }
+}
 
 startButton.addEventListener('click', startGame)
-   
+ 
+// Function to loop through the next question based on conditions
 function setNextQuestion () {
-  response.setAttribute('class', 'hidden')
   const currentQuestion = questionsList[questionCounter] 
  
   if (!currentQuestion) {
     secondsLeft===0;
     endPage();
+
   } else {
     let title = currentQuestion.question;
     questionElement.children[0].textContent = title;
@@ -117,12 +123,15 @@ function setNextQuestion () {
     }
 }
 
+// Listen for user input/clicks
 answer1Element.addEventListener('click', checkAnswer)
 answer2Element.addEventListener('click', checkAnswer)
 answer3Element.addEventListener('click', checkAnswer)
 answer4Element.addEventListener('click', checkAnswer)
 
+// Check user choice against correct answer
 function checkAnswer(event) {
+  response.textContent = " "
   const currentQuestion = questionsList[questionCounter] 
   const selectedButton = event.target;
   console.log(event.target.textContent);
@@ -131,84 +140,62 @@ function checkAnswer(event) {
   score++;
   console.log(score)
   questionCounter++;
+  response.textContent = 'Correct!'
   console.log(questionCounter);
-  response.children[0].textContent = "Correct";
   setNextQuestion();
   }
   else {
   secondsLeft -= 15;
-  response.children[0].textContent = "Wrong. Try again";
+  response.textContent = 'Wrong, try again!';
   }
-  }
+}
+
+// When time is up or questions are all answered (whichever comes first), display score and request intitials
+function endPage() {
+  answerButtonsElement.setAttribute('class', 'hidden')
+  countdownClock.textContent = 'Time up!';
+  countdownClock.setAttribute('class', 'hidden');
+  questionSectionElement.textContent = `You're done! \nYour final score is ${score} out of 5.`
+  formElement.setAttribute('class', ' ');
+} 
+submitButton = document.querySelector('#submit');
+scoresList.setAttribute('class', ' ');
+scoreboard = document.getElementById('scoreboard')
+
+/// Set and retreive scores to the local storage 
+// Currently, the function logs the score and initials in local storage, but
+// does not successfully render the scores to the page
+
+function renderLastScores() {
+  var initials = window.localStorage.getItem('initials');
+  var score = local.Storage.getItem('score');
+
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var initials = document.querySelector('#initials').value;
+  
+  window.localStorage.setItem('initials', initials);
+  localStorage.setItem('score', score);
  
-  function endPage() {
-    answerButtonsElement.setAttribute('class', 'hidden')
-    countdownClock.textContent = 'Time up!';
-    questionSectionElement.textContent = `You're done! \nYour final score is ${score} out of 5.`
-    formElement.setAttribute('class', 'block');
-
-
-  } 
-    
-
-var highscore = 0
-var storedHighScore = localStorage.getItem("highscore");
-if (storedHighScore  || score > parseInt(storedHighScore)) {
-  localStorage.setItem("highscore", score);
-}
-function highScores() {
-    var listEl = document.createElement("ol");
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
-    var li5 = document.createElement("li");
-    form.addEventListener('submit', (event) => {
-    
-    });
-
+  renderLastScores();
+})
 }
 
-/////////////////////////
-// var student = document.getElementById("student-names");
-// var grade = document.getElementById("grades");
-// var comment = document.getElementById("msg");
-// var saveButton = document.getElementById("save");
-// var savedName = document.getElementById("saved-name");
 
-// function saveLastGrade() {
-//   // Save related form data as an object
-//   var studentGrade = {
-//     student: student.value,
-//     grade: grade.value,
-//     comment: comment.value.trim()
-//   };
-//   // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-//   localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// }
 
-// function renderLastGrade() {
-//   // Use JSON.parse() to convert text to JavaScript object
-//   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-//   // Check if data is returned, if not exit out of the function
-//   if (lastGrade !== null) {
-//   document.getElementById("saved-name").innerHTML = lastGrade.student;
-//   document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-//   document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-//   } else {
+// function getLastScores() {
+//   var initials = localStorage.getItem("initials");
+//   var score = localStorage.getItem("score");
+
+//   if (!initials || !score) {
 //     return;
 //   }
+//   
 // }
 
-// saveButton.addEventListener("click", function(event) {
-// event.preventDefault();
-// saveLastGrade();
-// renderLastGrade();
+// scoreButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     getLastScores();
 // });
 
-// // The init() function fires when the page is loaded 
-// function init() {
-//   // When the init function is executed, the code inside renderLastGrade function will also execute
-//   renderLastGrade();
-// }
-// init();
